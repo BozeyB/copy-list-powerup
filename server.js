0b9de9ac303b3c081c78b21731cd8c3848e036f8
Glitch (hello-express) <none>
2025-07-02 19:25:25 +0000

    🎈🙍 Checkpoint ./public/client.js:82310304/1786 ./public/index.html:82310304/1837 ./server.js:82310304/1924 ./LICENSE.md:82310304/1068 ./package.json:82310304/3874 ./manifest.json:82310304/297

--- server.js
+++ server.js
@@ -1,22 +1,49 @@
-// server.js
-// where your node app starts
-
-// init project
 const express = require("express");
 const app = express();
+const axios = require("axios");
+require("dotenv").config();
 
-// we've started you off with Express,
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function(request, response) {
-  response.sendFile(__dirname + "/views/index.html");
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
 });
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
-  console.log("Your app is listening on port " + listener.address().port);
+// Example backend endpoint to copy cards from one list to another
+app.get("/copy-list", async (req, res) => {
+  const { sourceListId, targetListId } = req.query;
+
+  if (!sourceListId || !targetListId) {
+    return res.status(400).send("Missing list IDs");
+  }
+
+  try {
+    const cards = await axios.get(`https://api.trello.com/1/lists/${sourceListId}/cards`, {
+      params: {
+        key: process.env.TRELLO_API_KEY,
+        token: process.env.TRELLO_TOKEN,
+      },
+    });
+
+    for (const card of cards.data) {
+      await axios.post(`https://api.trello.com/1/cards`, null, {
+        params: {
+          name: card.name,
+          desc: card.desc,
+          idList: targetListId,
+          key: process.env.TRELLO_API_KEY,
+          token: process.env.TRELLO_TOKEN,
+        },
+      });
+    }
+
+    res.send("Cards copied successfully");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error copying cards");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Power-Up server is running on port 3000");
 });
