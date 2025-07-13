e3c8b6c9c34b99aabe1accc4bbea3c8aec73566e
Glitch (hello-express) <none>
2025-07-13 14:32:03 -0400

    Added Trello user-boards API route

--- api/copy-to-many.js
+++ api/copy-to-many.js
@@ -29,6 +29,7 @@ export default async function handler(req, res) {
 
     res.status(200).send("Cards copied successfully.");
   } catch (err) {
+    
     console.error(err);
     res.status(500).send("Error copying cards.");
   }
