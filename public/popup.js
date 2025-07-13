fce68c452701105c78b81e3b78961206ea43e229
Glitch (hello-express) <none>
2025-07-13 14:28:25 -0400

    Removed hardcoded Trello credentials

--- public/popup.js
+++ public/popup.js
@@ -24,12 +24,9 @@ document.addEventListener("DOMContentLoaded", async function () {
     opt.textContent = list.name;
     sourceListSelect.appendChild(opt);
   });
-
-  apiKey = process.env.TRELLO_API_KEY;
-  token = process.env.TRELLO_API_TOKEN;
  
   try {
-    const res = await fetch(`https://copy-list-powerup.vercel.app/api/user-boards?apiKey=${apiKey}&token=${token}`);
+    const res = await fetch(`https://copy-list-powerup.vercel.app/api/user-boards?apiKey=${process.env.TRELLO_API_KEY}&token=${process.env.TRELLO_API_TOKEN}`);
     const boards = await res.json();
 
     boards.forEach(board => {
