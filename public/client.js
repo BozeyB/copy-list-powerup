ce104f00d6b79887bfd414ea74d6ff2454361069
Glitch (hello-express) <none>
2025-07-13 14:43:34 -0400

    Removed .env from tracking

--- public/client.js
+++ public/client.js
@@ -7,13 +7,26 @@ function showCopyListPopup(t) {
   });
 }
 
+function showSettingsPopup(t) {
+  return t.popup({
+    title: "Configure Trello API",
+    url: "./settings.html",
+    height: 200
+  });
+}
+
 window.TrelloPowerUp.initialize({
-  "board-buttons": function (t, options) {
+  'board-buttons': function(t, options) {
     return [
       {
         icon: "https://cdn-icons-png.flaticon.com/512/1828/1828859.png",
         text: "Copy List",
         callback: showCopyListPopup
+      },
+      {
+        icon: "https://cdn-icons-png.flaticon.com/512/1828/1828859.png",
+        text: "Settings",
+        callback: showSettingsPopup
       }
     ];
   }
