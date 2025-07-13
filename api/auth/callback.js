52a732685e94fa5a047b7a4419e6ac8597d0434d
Glitch (hello-express) <none>
2025-07-13 14:57:18 -0400

    Removed .env from tracking

--- api/auth/callback.js
+++ api/auth/callback.js
@@ -1,1 +1,14 @@
-callback.html (or you can make /auth/callback an API route)
+export default function handler(req, res) {
+  res.setHeader("Content-Type", "text/html");
+  res.status(200).send(`
+    <html>
+      <head><title>Authorization Complete</title></head>
+      <body>
+        <h2>Authorization successful. You may close this window.</h2>
+        <script>
+          window.TrelloPowerUp && window.TrelloPowerUp.iframe().closeModal();
+        </script>
+      </body>
+    </html>
+  `);
+}
